7c10ae4c81e961d8d99f84f4416a6d11732b42e2
elmirsurkeev <elmirsurkeev@gmail.com>
2026-04-07 13:12:52 +0600

    feat: initial project setup with Spring Boot 3.5.13

---
+++ src/main/java/com/elmirsurkeev/taskmanager/TaskflowApplication.java
@@ -0,0 +1,50 @@
+package com.elmirsurkeev.taskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskflowApplication.class, args);
+	}
+
+}
+
+//
+//com.yourname.taskmanager
+//├── controller
+//├── service
+//├── repository
+//├── domain
+//│   ├── entity
+//│   └── enums
+//├── dto
+//│   ├── request
+//│   └── response
+//├── exception
+//├── config
+//└── mapper
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
